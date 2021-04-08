4b65e8d0aca4032b9c77d7a97f09071e0d89daa7
wilsonrivero <wilsonrivero2002@gmail.com>
2021-04-07 22:13:07 -0300

    Add comments in main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 
 pygame.init()
 
+## VARIABLES GLOBAL
 BLACK = (0,0,0)
 WHITE = (255,255,255)
 WIDTH, HEIGHT = 600,600
@@ -15,25 +16,28 @@ health = 3
 run = True
 
 
-
+##SCREEN AND CAPTION
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Astroid game")
 
 img = pygame.image.load('./assets/images/background.jpg')
 new_img = pygame.transform.scale(img, (WIDTH, HEIGHT))
 
-
+#BACKGROUND SOUND
 mixer.music.load('./assets/audio/background.wav')
 mixer.music.play(-1)
 
+#BULLET SOUND AND EXPLOSION SOUND 
 bullet_sound = mixer.Sound('./assets/audio/laser.wav')
 explosion_sound = mixer.Sound('./assets/audio/explosion.wav')
 
-
+#FONT 
 font = pygame.font.Font('./assets/fonts/FiraCode-Bold.ttf', 25)
 
+#FPS
 clock = pygame.time.Clock()
 
+#Create spaceship with sprite 
 class SpaceShip(pygame.sprite.Sprite):
    def __init__(self, bullets):
       super().__init__()
@@ -46,6 +50,7 @@ class SpaceShip(pygame.sprite.Sprite):
 
       self.rect = self.image.get_rect(center=(self.x, self.y))
 
+   # Moving the spaceship
    def update(self):
       keys = pygame.key.get_pressed()
 
@@ -73,7 +78,7 @@ class SpaceShip(pygame.sprite.Sprite):
    def create_fire(self):
       self.bullets.add(Bullets(*self.rect.center))
 
-
+#Create bullets sprites
 class Bullets(pygame.sprite.Sprite):
    def __init__(self, x, y):
       super().__init__()
@@ -81,12 +86,13 @@ class Bullets(pygame.sprite.Sprite):
       self.image = pygame.image.load('./assets/images/bullet.png')
       self.rect = self.image.get_rect(center=(x, y - 15))
 
+   #Moving bullets
    def update(self):
       self.rect.y -= 7
       if self.rect.y < 0:
          self.kill()
 
-
+#Create Enemies
 class Enemies(pygame.sprite.Sprite):
    def __init__(self):
       super().__init__()
@@ -94,6 +100,7 @@ class Enemies(pygame.sprite.Sprite):
       self.image = pygame.image.load(f'./assets/images/monster{self.imgs}.png')
       self.rect = self.image.get_rect(center=(random.randint(10, 560), -10))
 
+   #Moving the Enemies
    def update(self):
       global health
 
@@ -111,28 +118,32 @@ class Enemies(pygame.sprite.Sprite):
 
 
 
-
+#Gruops fo sprites
 group_spaceship = pygame.sprite.Group()
 group_bullets = pygame.sprite.Group()
 group_enemies = pygame.sprite.Group()
 group_hearts = pygame.sprite.Group()
 
+#Instânciando uma classs
 spaceship = SpaceShip(group_bullets)
 enemies = Enemies()
 
+# Add class in a group 
 group_spaceship.add(spaceship)
 group_enemies.add(enemies)
 
+#Sart Game
 while run:
    clock.tick(30)
    
-
+   # Creating a monster in game 
    if round % 100 == 0:
       group_enemies.add(Enemies())
-
+      
+      #Let more hard the game
       if score > 10:
          group_enemies.add(Enemies())
-         
+
          if score > 20:
             group_enemies.add(Enemies())
 
@@ -140,32 +151,38 @@ while run:
                group_enemies.add(Enemies())
 
 
+   # events in pygame
    for event in pygame.event.get():
       if event.type == pygame.QUIT:
          run = False
          pygame.quit() 
 
+      #Shooting 
       if event.type == pygame.KEYDOWN:
          if event.key == pygame.K_SPACE:
             bullet_sound.play()
             spaceship.create_fire()
       
-      
+   #Collision with the bullets and monster if collisoin is true destroy bullet and monster and play sound of explosion
    if pygame.sprite.groupcollide(group_bullets, group_enemies, True, True):
       explosion_sound.play()
       score += 1
 
+   # Make a a score in screen
    text = font.render(f'Score:{score}', False, WHITE)
    
-
+   # Render a background image in screen
    screen.blit(new_img, (0,0))
+
+   # Render a score on screen
    screen.blit(text, (460, 30))
    
-   
+   # Draw a sprites   
    group_spaceship.draw(screen)
    group_bullets.draw(screen)
    group_enemies.draw(screen)
 
+   # UPdates sprites
    group_spaceship.update()
    group_bullets.update()
    group_enemies.update()
